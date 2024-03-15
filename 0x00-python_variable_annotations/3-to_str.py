e76a56a9cb9692dcb5a316d9294767e6947a5c37
Ernest-Geek <ernestampene1@gmail.com>
2024-03-15 23:20:17 +0000

    update

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-def to_str(n: float):
-    #return the string representation of the float
+def to_str(n: float) -> float:
+    """return the string representation of the float"""
     return str(n)
